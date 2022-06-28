c010d4a71df6f8cbbb3a407839cc33daf596b028
Sharon Odima <odimasharon@gmail.com>
2022-06-27 22:35:36 -0400

    Sharon Odima Unit4.3Module2 Project Initial Commit

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,9 @@ function find() {
       }
     ]
    */
+  return db("users")
+    .join("roles", "users.role_id", "roles.role_id")
+    .select("user_id", "username", "role_name")
 }
 
 function findBy(filter) {
@@ -34,6 +37,10 @@ function findBy(filter) {
       }
     ]
    */
+    return db("users")
+    .join("roles", "users.role_id", "roles.role_id")
+    .select("user_id", "username", "password", "role_name")
+    .where(filter)
 }
 
 function findById(user_id) {
