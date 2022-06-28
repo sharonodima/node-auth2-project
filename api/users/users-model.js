6272c1c22349d2b5427ff2ae900502084e4a108a
Sharon Odima <odimasharon@gmail.com>
2022-06-28 19:31:41 -0400

    Sharon Odima Completed Unit4.3Module2 Project with some tests passing

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -39,7 +39,7 @@ function findBy(filter) {
    */
     return db("users")
     .join("roles", "users.role_id", "roles.role_id")
-    .select("user_id", "username", "password", "role_name")
+    .select("user_id", "username", "role_name")
     .where(filter)
 }
 
@@ -54,6 +54,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db("users")
+    .join("roles", "users.role_id", "roles.role_id")
+    .select("user_id", "username", "role_name")
+    .where("users.user_id", user_id).first()
 }
 
 /**
